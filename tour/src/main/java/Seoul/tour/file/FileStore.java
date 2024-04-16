5866c6f2d6dfae5e5a6ec1256dcd812e537f8a0c
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2024-04-16 16:19:28 +0900

    Add files via upload

---
+++ tour/src/main/java/Seoul/tour/file/FileStore.java
@@ -0,0 +1,112 @@
+package Seoul.tour.file;
+
+
+import Seoul.tour.domain.UploadFile;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.io.File;
+
+@Component
+public class FileStore {
+
+//    @Value("${file.dir}")
+//    private String fileDir;
+//
+//    public String getFullPath(String filename) {
+//        return fileDir + filename;
+//    }
+//
+//    public List<UploadFile> storeFiles(List<MultipartFile> multipartFiles)
+//            throws IOException {
+//        List<UploadFile> storeFileResult = new ArrayList<>();
+//        for (MultipartFile multipartFile : multipartFiles) {
+//            if (!multipartFile.isEmpty()) {
+//                storeFileResult.add(storeFile(multipartFile));
+//            }
+//        }
+//        return storeFileResult;
+//    }
+//
+//    public UploadFile storeFile(MultipartFile multipartFile) throws IOException {
+//
+//        if (multipartFile.isEmpty()) {
+//            return null;
+//        }
+//
+//        String originalFilename = multipartFile.getOriginalFilename();
+//        String storeFileName = createStoreFileName(originalFilename);
+//        multipartFile.transferTo(new File(getFullPath(storeFileName)));
+//        return new UploadFile(originalFilename, storeFileName);
+//    }
+//
+//    private String createStoreFileName(String originalFilename) {
+//        String ext = extractExt(originalFilename);
+//        String uuid = UUID.randomUUID().toString();
+//        return uuid + "." + ext;
+//    }
+//
+//    private String extractExt(String originalFilename) {
+//        int pos = originalFilename.lastIndexOf(".");
+//        return originalFilename.substring(pos + 1);
+//    }
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    public String getFullPath(String filename) {
+        return fileDir + filename;
+    }
+
+    public List<UploadFile> storeFiles(List<MultipartFile> multipartFiles)
+            throws IOException {
+        List<UploadFile> storeFileResult = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFiles) {
+            if (!multipartFile.isEmpty()) {
+                storeFileResult.add(storeFile(multipartFile));
+            }
+        }
+        return storeFileResult;
+    }
+
+    public UploadFile storeFile(MultipartFile multipartFile) throws IOException {
+        if (multipartFile.isEmpty()) {
+            return null;
+        }
+
+        // 디렉토리 생성
+        createDirectoryIfNeeded();
+
+        String originalFilename = multipartFile.getOriginalFilename();
+        String storeFileName = createStoreFileName(originalFilename);
+        multipartFile.transferTo(new File(getFullPath(storeFileName)));
+        return new UploadFile(originalFilename, storeFileName);
+    }
+
+    private String createStoreFileName(String originalFilename) {
+        String ext = extractExt(originalFilename);
+        String uuid = UUID.randomUUID().toString();
+        return uuid + "." + ext;
+    }
+
+    private String extractExt(String originalFilename) {
+        int pos = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(pos + 1);
+    }
+
+    private void createDirectoryIfNeeded() {
+        File directory = new File(fileDir);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+    }
+}
+
+
+
+
