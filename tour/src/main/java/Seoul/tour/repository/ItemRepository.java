8a19b81bbc697822105ece2c78dc83458962393c
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2024-04-17 16:25:55 +0900

    Add files via upload

---
+++ tour/src/main/java/Seoul/tour/repository/ItemRepository.java
@@ -0,0 +1,23 @@
+package Seoul.tour.repository;
+
+
+import Seoul.tour.domain.Item;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private final Map<Long, Item> store = new HashMap<>();
+    private long sequence = 0L;
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+}
